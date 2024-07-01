421cebfc56587241c165822509c63dd52dc242cf
PedroVladimir <pvfloresriosgmail.com>
2024-06-30 22:41:24 -0400

    ultimas correciones en titulos cypress

--- cypress/e2e/flujo alterno/1fa-sistema.spec.cy.ts
+++ cypress/e2e/flujo alterno/1fa-sistema.spec.cy.ts
@@ -1,4 +1,4 @@
-describe('Prueba que la app esta levantadada en el servidor local con el puero 3001', () => {
+describe('Prueba que la app esta levantada en el servidor local con el puerto 3001', () => {
     it('passes', () => {
       cy.visit('http://localhost:3001')
       cy.contains('Productos').should('be.visible')
